4a444b067fef8299969431d18ab9f6bbf1dd4d04
Dunghthe04 <tuandungyz09@gmail.com>
2025-05-31 12:38:01 +0700

    DUng-CRUD(Restaurant,Entertainment)-31-5

--- src/java/controller/agent/entertainment/ManagementEntertainment.java
+++ src/java/controller/agent/entertainment/ManagementEntertainment.java
@@ -1,25 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
-package controller.user;
+package controller.agent.entertainment;
 
+import dao.EntertainmentDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import service.EmailSender;
+import java.util.List;
+import model.Entertainment;
 
 /**
  *
- * @author Nhat Anh
+ * @author ad
  */
-@WebServlet(name = "ResendOtp", urlPatterns = {"/ResendOtp"})
-public class ResendOtp extends HttpServlet {
+public class ManagementEntertainment extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -33,31 +29,18 @@ public class ResendOtp extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        HttpSession session = request.getSession();
-
-        //lấy email từ session (giả sử đa lưu email vào session từ trc dó)
-        String gmail = (String) session.getAttribute("gmail");
-        if (gmail == null) {
-            response.sendRedirect(request.getContextPath() + "/view/user/verify-otp.jsp?error=Không tìm thấy email trong session!");
-            return;
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ManagementEntertainment</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ManagementEntertainment at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
         }
-        //Tạo otp mới
-        int otp = (int) (Math.random() * 900000) + 100000;
-        long expiryTime = System.currentTimeMillis() + 2 * 60 * 1000;
-        session.setAttribute("otp", otp);
-        session.setAttribute("otpExpiry", expiryTime);
-        session.setAttribute("otpFails", 0);
-
-        //Gửi gmail lại 
-       try {
-        EmailSender.send(gmail, "Mã OTP mới của bạn", "Mã OTP mới của bạn là: " + otp);
-        request.setAttribute("message", "Mã OTP mới đã được gửi!");
-        request.getRequestDispatcher("/view/user/verify-otp.jsp").forward(request, response);
-    } catch (Exception e) {
-        request.setAttribute("error", "Lỗi khi gửi email. Vui lòng thử lại!");
-        request.getRequestDispatcher("/view/user/verify-otp.jsp").forward(request, response);
-    }
-
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -72,7 +55,21 @@ public class ResendOtp extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        HttpSession session = request.getSession();
+        List<Entertainment> listEnt;
+        EntertainmentDAO entDao = new EntertainmentDAO();
+        String findByType = request.getParameter("type");
+        if (findByType != null && !findByType.isEmpty()) {
+            listEnt = entDao.getEntertainmentByType(findByType);
+            session.setAttribute("listEnt", listEnt);
+            request.getRequestDispatcher("view/agent/entertainment/AgentEntertainment.jsp").forward(request, response);
+            return;
+        }
+        listEnt = entDao.getListEntertainment();
+        session.setAttribute("listEnt", listEnt);
+        session.setAttribute("status", findByType);
+        request.getRequestDispatcher("view/agent/entertainment/AgentEntertainment.jsp").forward(request, response);
+
     }
 
     /**
---
+++ src/java/dao/EntertainmentDAO.java
@@ -0,0 +1,265 @@
+package dao;
+
+import com.sun.jdi.connect.spi.Connection;
+import dal.DBContext;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import model.Entertainment;
+
+public class EntertainmentDAO extends DBContext {
+
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public int insertService(String serviceName) throws SQLException {
+        String sql = "INSERT INTO [dbo].[Service]\n"
+                + "           ([serviceCategoryID])\n"
+                + "     VALUES\n"
+                + "           (?)";
+        PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        ps.setInt(1, 2); // Ví dụ: 2 là mã dịch vụ giải trí
+        ps.executeUpdate();
+
+        ResultSet rs = ps.getGeneratedKeys();
+        if (rs.next()) {
+            return rs.getInt(1);
+        } else {
+            throw new SQLException("Không thể lấy serviceID sau khi insert Service.");
+        }
+    }
+
+    public void insertEntertainmentFull(String name, String image, String address, String phone, String description, float rate, String type, int status, String timeOpen, String timeClose, String dayOfWeekOpen, float ticketPrice) {
+        try {
+            connection.setAutoCommit(false);
+            int serviceID = insertService("Dịch vụ giải trí");
+
+            String sql = "INSERT INTO [dbo].[Entertainment]\n"
+                    + "           ([serviceID]\n"
+                    + "           ,[name]\n"
+                    + "           ,[image]\n"
+                    + "           ,[address]\n"
+                    + "           ,[phone]\n"
+                    + "           ,[description]\n"
+                    + "           ,[rate]\n"
+                    + "           ,[type]\n"
+                    + "           ,[status]\n"
+                    + "           ,[timeOpen]\n"
+                    + "           ,[timeClose]\n"
+                    + "           ,[dayOfWeekOpen]\n"
+                    + "           ,[ticketPrice])\n"
+                    + "     VALUES\n"
+                    + "           (?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            ps = connection.prepareStatement(sql);
+            ps.setInt(1, serviceID);
+            ps.setString(2, name);
+            ps.setString(3, image);
+            ps.setString(4, address);
+            ps.setString(5, phone);
+            ps.setString(6, description);
+            ps.setFloat(7, rate);
+            ps.setString(8, type);
+            ps.setInt(9, status);
+            ps.setTime(10, Time.valueOf(timeOpen));
+            ps.setTime(11, Time.valueOf(timeClose));
+            ps.setString(12, dayOfWeekOpen);
+            ps.setFloat(13, ticketPrice);
+            ps.executeUpdate();
+            connection.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Entertainment> getListEntertainment() {
+        String sql = "SELECT * FROM Entertainment";
+        List<Entertainment> list = new ArrayList<>();
+        try {
+            ps = connection.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Time timeOpen = rs.getTime("timeOpen");
+                Time timeClose = rs.getTime("timeClose");
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+                Entertainment ent = new Entertainment(
+                        rs.getInt("serviceID"),
+                        rs.getString("name"),
+                        rs.getString("image"),
+                        rs.getString("address"),
+                        rs.getString("phone"),
+                        rs.getString("description"),
+                        rs.getFloat("rate"),
+                        rs.getString("type"),
+                        rs.getInt("status"),
+                        sdf.format(timeOpen),
+                        sdf.format(timeClose),
+                        rs.getString("dayOfWeekOpen"),
+                        rs.getFloat("ticketPrice")
+                );
+                list.add(ent);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return list;
+    }
+
+    public Entertainment getEntertainmentByServiceId(int serviceId) {
+        String sql = "SELECT * FROM Entertainment WHERE serviceID=?";
+        try {
+            ps = connection.prepareStatement(sql);
+            ps.setInt(1, serviceId);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                Time timeOpen = rs.getTime("timeOpen");
+                Time timeClose = rs.getTime("timeClose");
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+                return new Entertainment(
+                        rs.getInt("serviceID"),
+                        rs.getString("name"),
+                        rs.getString("image"),
+                        rs.getString("address"),
+                        rs.getString("phone"),
+                        rs.getString("description"),
+                        rs.getFloat("rate"),
+                        rs.getString("type"),
+                        rs.getInt("status"),
+                        sdf.format(timeOpen),
+                        sdf.format(timeClose),
+                        rs.getString("dayOfWeekOpen"),
+                        rs.getFloat("ticketPrice")
+                );
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    public void updateEntertainment(int serviceId, String name, String image, String address, String phone,
+            String description,float rate, String type, int status, String timeOpen,
+            String timeClose, String dayOfWeekOpen, double ticketPrice) {
+        String sql = "UPDATE [dbo].[Entertainment]\n"
+                + "   SET [name] = ?\n"
+                + "      ,[image] = ?\n"
+                + "      ,[address] = ?\n"
+                + "      ,[phone] = ?\n"
+                + "      ,[description] = ?\n"
+                + "      ,[rate] = ?\n"
+                + "      ,[type] = ?\n"
+                + "      ,[status] = ?\n"
+                + "      ,[timeOpen] = ?\n"
+                + "      ,[timeClose] = ?\n"
+                + "      ,[dayOfWeekOpen] = ?\n"
+                + "      ,[ticketPrice] = ?\n"
+                + " WHERE serviceID=?";
+        try {
+            ps = connection.prepareStatement(sql);
+            ps.setString(1, name);
+            ps.setString(2, image);
+            ps.setString(3, address);
+            ps.setString(4, phone);
+            ps.setString(5, description);
+            ps.setFloat(6, rate);
+            ps.setString(7, type);
+            ps.setInt(8, status);
+            ps.setTime(9, Time.valueOf(timeOpen));
+            ps.setTime(10, Time.valueOf(timeClose));
+            ps.setString(11, dayOfWeekOpen);
+            ps.setDouble(12, ticketPrice);
+            ps.setInt(13, serviceId);
+            ps.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void deleteEntertainment(int serviceID) {
+        String sqlEntertainment = "DELETE FROM Entertainment WHERE serviceID=?";
+        String sqlService = "DELETE FROM Service WHERE serviceID=?";
+        try {
+            PreparedStatement psEnt = connection.prepareStatement(sqlEntertainment);
+            psEnt.setInt(1, serviceID);
+            psEnt.executeUpdate();
+
+            PreparedStatement psServ = connection.prepareStatement(sqlService);
+            psServ.setInt(1, serviceID);
+            psServ.executeUpdate();
+        } catch (Exception e) {
+            System.out.println("Error deleting entertainment: " + e.getMessage());
+        }
+    }
+
+    public List<Entertainment> searchEntertainmentByName(String name) {
+        String sql = "select * from Entertainment where type like N'%"+name+"%'";
+        List<Entertainment> list = new ArrayList<>();
+        try {
+            ps = connection.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Time timeOpen = rs.getTime("timeOpen");
+                Time timeClose = rs.getTime("timeClose");
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+                Entertainment ent = new Entertainment(
+                        rs.getInt("serviceID"),
+                        rs.getString("name"),
+                        rs.getString("image"),
+                        rs.getString("address"),
+                        rs.getString("phone"),
+                        rs.getString("description"),
+                        rs.getFloat("rate"),
+                        rs.getString("type"),
+                        rs.getInt("status"),
+                        sdf.format(timeOpen),
+                        sdf.format(timeClose),
+                        rs.getString("dayOfWeekOpen"),
+                        rs.getFloat("ticketPrice")
+                );
+                list.add(ent);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return list;
+    }
+
+    public List<Entertainment> getEntertainmentByType(String type) {
+        String sql = "SELECT * FROM Entertainment WHERE type LIKE ?";
+        List<Entertainment> list = new ArrayList<>();
+        try {
+            ps = connection.prepareStatement(sql);
+            ps.setString(1, "%" + type + "%");
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Time timeOpen = rs.getTime("timeOpen");
+                Time timeClose = rs.getTime("timeClose");
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+                Entertainment ent = new Entertainment(
+                        rs.getInt("serviceID"),
+                        rs.getString("name"),
+                        rs.getString("image"),
+                        rs.getString("address"),
+                        rs.getString("phone"),
+                        rs.getString("description"),
+                        rs.getFloat("rate"),
+                        rs.getString("type"),
+                        rs.getInt("status"),
+                        sdf.format(timeOpen),
+                        sdf.format(timeClose),
+                        rs.getString("dayOfWeekOpen"),
+                        rs.getFloat("ticketPrice")
+                );
+                list.add(ent);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return list;
+    }
+}
